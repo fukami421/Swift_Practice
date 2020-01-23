28cf40665a7425bc1a4881f2f371dc41383dbdba
Ryuichi Fukami <fukaryu421@gmail.com>
2020-01-23 19:59:53 +0900

    Initial Commit

---
+++ Swift_Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift_Practice
+//
+//  Created by 深見龍一 on 2020/01/23.
+//  Copyright © 2020 深見龍一. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
